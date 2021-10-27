4dc8598c23825f5e3ddc1e73789d9330d3b61bbf
bharathkumarbollena <Bharathlucky350@gmail.com>
2021-10-27 17:25:18 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    console.log(req.query)
+
+    if (Object.keys(req.query).length === 0) {
+        console.log("Entered");
+        let rand = Math.random();
+        res.render(`computation`, { value: `hypot applied to ${rand} is ${Math.hypot(rand)}` })
+    } else
+        for (let a in req.query) {
+            console.log(a);
+            res.render(`computation`, { value: `hypot applied to ${req.query[a]} is ${Math.hypot(req.query[a])}` })
+        }
+})
+
+module.exports = router
